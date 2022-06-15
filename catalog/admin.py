6c1f9dc127e1cc19698952ddb99f24f7c6d9ad93
Sean O'Gary <seanogarydev@gmail.com>
2022-06-15 14:13:51 -0400

    Part 4 challenge complete

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,19 +2,25 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
-# Define the admin class
+class BookInline(admin.TabularInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
 
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
@@ -25,6 +31,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -35,11 +42,3 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
-
-
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-
